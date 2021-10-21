4b8660b7916fa100006c30459ad269d0dab78a53
brayansuamon <brayansuamon@gmail.com>
2021-10-21 13:06:25 -0500

    Añadir Style-components

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,15 +1,16 @@
 export default function Header(){
     const [mostrarnocturno,setMostrarnocturno] = useState("false"); 
 
-    const manejarClick = () =>{
+    const manejarClick = (e) =>{
      setMostrarnocturno(true);
     }
     
     return (
         <header className="header">
         {mostrarnocturno ? 
-         (<img src="./images/logo-mobile-modo-noct.svg" alt="Modo Nocturno" />) : (<img src="./images/logo-desktop.svg" alt="Modo Diurno" />)}
-        <button value={mostrarnocturno} onClick={manejarClick}>
+         (<img src="./images/logo-mobile-modo-noct.svg" alt="Modo Nocturno" />) : 
+         (<img src="./images/logo-desktop.svg" alt="Modo Diurno" />)}
+        <button value={mostrarnocturno} onClick={manejarClick} className={`${mostrarnocturno ? darkmode : ligthmode} modos`}>
             {mostrarnocturno ? "MODO DARK" : "MODO LIGTH"}
         </button>
          </header>
