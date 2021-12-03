d26478a9cf2229788e9f18a01171c2d24d2485ce
brayansuamon <brayansuamon@gmail.com>
2021-12-02 20:38:19 -0500

    Final

--- src/components/cajagifs/gifs/Gifs.jsx
+++ src/components/cajagifs/gifs/Gifs.jsx
@@ -4,7 +4,9 @@ import "./Gifs.scss";
 export default function Gifs({ images: { downsized }, id }) {
   return (
     <div className="Gifs">
-      <img src={`${downsized.url}`} alt="Imagen" />
+      <a href={downsized.url}>
+        <img src={`${downsized.url}`} alt="Imagen" />
+      </a>
     </div>
   );
 }
